b4bea2d46da4c853898cd345873291d4ee68ec35
Ahmad Javaid <ahmadjavaiddev@gmail.com>
2024-04-11 17:53:16 +0500

    Refactor CSS classes for better readability and responsiveness

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -32,10 +32,13 @@ const Login = () => {
      const handleLogin = async (e) => {
           e.preventDefault();
           try {
-               const response = await axios.post(`https://truefeedback-backend.vercel.app/api/v1/users/login`, {
-                    email: userData.email,
-                    password: userData.password,
-               });
+               const response = await axios.post(
+                    `https://truefeedback-backend.vercel.app/api/v1/users/login`,
+                    {
+                         email: userData.email,
+                         password: userData.password,
+                    }
+               );
 
                if (response.data.data) {
                     setUserData({
@@ -53,63 +56,71 @@ const Login = () => {
      };
 
      return (
-          <div className="mt-10">
-               <form className="max-w-sm mx-auto" onSubmit={handleLogin}>
-                    <div className="mb-5">
-                         <label
-                              htmlFor="email"
-                              className="block mb-2 text-sm font-medium text-gray-900"
+          <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+               <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                         <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+                              Sign in to your account
+                         </h1>
+                         <form
+                              className="space-y-4 md:space-y-6"
+                              onSubmit={handleLogin}
                          >
-                              Your email
-                         </label>
-                         <input
-                              type="email"
-                              name="email"
-                              value={userData.email}
-                              onChange={handleChange}
-                              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                              placeholder="name@name.com"
-                              required
-                         />
+                              <div>
+                                   <label
+                                        htmlFor="email"
+                                        className="block mb-2 text-sm font-medium text-gray-900"
+                                   >
+                                        Email
+                                   </label>
+                                   <input
+                                        type="email"
+                                        name="email"
+                                        id="email"
+                                        value={userData.email}
+                                        onChange={handleChange}
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 focus:ring-blue-500"
+                                        placeholder="name@company.com"
+                                        required
+                                   />
+                              </div>
+                              <div>
+                                   <label
+                                        htmlFor="password"
+                                        className="block mb-2 text-sm font-medium text-gray-900"
+                                   >
+                                        Password
+                                   </label>
+                                   <input
+                                        type="password"
+                                        name="password"
+                                        id="password"
+                                        placeholder="••••••••"
+                                        value={userData.password}
+                                        onChange={handleChange}
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                                        required
+                                   />
+                              </div>
+
+                              <button
+                                   type="submit"
+                                   className="w-full text-white bg-[#0F172A] hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                              >
+                                   Sign in
+                              </button>
+                              <p className="text-sm font-light text-gray-500">
+                                   Don’t have an account yet?{" "}
+                                   <Link
+                                        to="/register"
+                                        className="font-medium text-primary-600 hover:underline"
+                                   >
+                                        Sign up
+                                   </Link>
+                              </p>
+                         </form>
                     </div>
-                    <div className="mb-5">
-                         <label
-                              htmlFor="password"
-                              className="block mb-2 text-sm font-medium text-gray-900"
-                         >
-                              Your password
-                         </label>
-                         <input
-                              type="password"
-                              name="password"
-                              value={userData.password}
-                              onChange={handleChange}
-                              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                              autoComplete="true"
-                              required
-                         />
-                    </div>
-                    <div className="flex justify-between items-center mb-5">
-                         <Link
-                              to={"/forgot-password"}
-                              className="text-sm text-gray-500 hover:text-gray-700"
-                         >
-                              Forgot Password
-                         </Link>
-                         <Link
-                              to={"/register"}
-                              className="ml-3 text-sm text-blue-500 hover:text-gray-700"
-                         >
-                              Create Account
-                         </Link>
-                    </div>
-                    <button
-                         type="submit"
-                         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
-                    >
-                         Submit
-                    </button>
-               </form>
+               </div>
           </div>
      );
 };
--- src/pages/signup/SignUp.jsx
+++ src/pages/signup/SignUp.jsx
@@ -8,7 +8,6 @@ import toast from "react-hot-toast";
 const SignUpPage = () => {
      const [userData, setUserData] = useState({
           username: "",
-          fullName: "",
           email: "",
           password: "",
      });
@@ -63,7 +62,6 @@ const SignUpPage = () => {
      useEffect(() => {
           if (
                userData.username.length > 2 &&
-               userData.fullName.length > 1 &&
                userData.email.length > 7 &&
                userData.password.length > 7
           ) {
@@ -71,106 +69,97 @@ const SignUpPage = () => {
           } else {
                setButtonDisabled(true);
           }
-     }, [
-          userData.username,
-          userData.fullName,
-          userData.email,
-          userData.password,
-     ]);
+     }, [userData.username, userData.email, userData.password]);
 
      return (
-          <div className="mt-10">
-               <form className="max-w-sm mx-auto" onSubmit={handleSignUp}>
-                    <div className="mb-5">
-                         <label
-                              htmlFor="fullName"
-                              className="block mb-2 text-sm font-medium text-gray-900"
+          <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+               <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                         <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+                              Sign Up
+                         </h1>
+                         <form
+                              className="space-y-4 md:space-y-6"
+                              onSubmit={handleSignUp}
                          >
-                              Your FullName
-                         </label>
-                         <input
-                              type="text"
-                              name="fullName"
-                              value={userData.fullName}
-                              onChange={handleChange}
-                              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                              placeholder="Jhon Doe"
-                              required
-                         />
-                    </div>
-                    <div className="mb-5">
-                         <label
-                              htmlFor="username"
-                              className="block mb-2 text-sm font-medium text-gray-900"
-                         >
-                              Your UserName
-                         </label>
-                         <input
-                              type="text"
-                              name="username"
-                              value={userData.username}
-                              onChange={handleChange}
-                              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                              placeholder="jhon_doe"
-                              required
-                         />
-                    </div>
-                    <div className="mb-5">
-                         <label
-                              htmlFor="email"
-                              className="block mb-2 text-sm font-medium text-gray-900"
-                         >
-                              Your email
-                         </label>
-                         <input
-                              type="email"
-                              name="email"
-                              value={userData.email}
-                              onChange={handleChange}
-                              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                              placeholder="name@name.com"
-                              required
-                         />
-                    </div>
-                    <div className="mb-5">
-                         <label
-                              htmlFor="password"
-                              className="block mb-2 text-sm font-medium text-gray-900"
-                         >
-                              Your password
-                         </label>
-                         <input
-                              type="password"
-                              name="password"
-                              value={userData.password}
-                              onChange={handleChange}
-                              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                              autoComplete="true"
-                              required
-                         />
-                    </div>
-                    <div className="flex items-start mb-5">
-                         <Link
-                              to={"/login"}
-                              className="text-sm text-gray-500 hover:text-gray-700"
-                         >
-                              Have an account?{" "}
-                              <span className="text-blue-500">Sign In</span>
-                         </Link>
-                    </div>
+                              <div>
+                                   <label
+                                        htmlFor="username"
+                                        className="block mb-2 text-sm font-medium text-gray-900"
+                                   >
+                                        Username
+                                   </label>
+                                   <input
+                                        type="text"
+                                        name="username"
+                                        id="username"
+                                        value={userData.username}
+                                        onChange={handleChange}
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 focus:ring-blue-500"
+                                        placeholder="name@company.com"
+                                        required
+                                   />
+                              </div>
+                              <div>
+                                   <label
+                                        htmlFor="email"
+                                        className="block mb-2 text-sm font-medium text-gray-900"
+                                   >
+                                        Email
+                                   </label>
+                                   <input
+                                        type="email"
+                                        name="email"
+                                        id="email"
+                                        value={userData.email}
+                                        onChange={handleChange}
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 focus:ring-blue-500"
+                                        placeholder="name@company.com"
+                                        required
+                                   />
+                              </div>
+                              <div>
+                                   <label
+                                        htmlFor="password"
+                                        className="block mb-2 text-sm font-medium text-gray-900"
+                                   >
+                                        Password
+                                   </label>
+                                   <input
+                                        type="password"
+                                        name="password"
+                                        id="password"
+                                        placeholder="••••••••"
+                                        value={userData.password}
+                                        onChange={handleChange}
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                                        required
+                                   />
+                              </div>
 
-                    <button
-                         type="submit"
-                         className={
-                              buttonDisabled
-                                   ? `text-white bg-gray-400 focus:outline-none font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center`
-                                   : `text-white bg-[#0F172A] hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center`
-                         }
-                         disabled={buttonDisabled}
-                    >
-                         Submit
-                    </button>
-               </form>
+                              <button
+                                   type="submit"
+                                   className={
+                                        buttonDisabled
+                                             ? "w-full text-white bg-gray-500 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                                             : "w-full text-white bg-[#0F172A] hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                                   }
+                                   disabled={buttonDisabled}
+                              >
+                                   Sign Up
+                              </button>
+                              <p className="text-sm font-light text-gray-500">
+                                   Already have an account?{" "}
+                                   <Link
+                                        to="/login"
+                                        className="font-medium text-primary-600 hover:underline"
+                                   >
+                                        Sign In
+                                   </Link>
+                              </p>
+                         </form>
+                    </div>
+               </div>
           </div>
      );
 };
